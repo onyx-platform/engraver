85d77e44758651571ee8a67df3a875ea1801b883
Michael Drogalis <mjd3089@rit.edu>
2016-03-05 11:02:33 -0800

    Store AWS keys in .engraver.

--- src/ansible.py
+++ src/ansible.py
@@ -11,6 +11,8 @@ def invoke_ansible(arg_vars, project_root, playbook):
   engraver_profile = expanduser("~") + "/.engraver"
   config.read(engraver_profile)
 
+  aws_access_key = config.get('aws', 'aws_access_key', 0)
+  aws_secret_key = config.get('aws', 'aws_secret_key', 0)
   aws_key_name = config.get('aws', 'aws_key_name', 0)
   pem_file_path = config.get('aws', 'pem_file_name', 0)
 
@@ -20,5 +22,7 @@ def invoke_ansible(arg_vars, project_root, playbook):
         "-i", ",", "-e", "remote_user='ubuntu'",
         "-e", ("cluster_name=" + arg_vars['cluster_name']),
         "-e", ("aws_key_name=" + aws_key_name),
+        "-e", ("aws_access_key=" + aws_access_key)
+        "-e", ("aws_secret_key=" + aws_secret_key)
         "-e", ("engraver_root=" + project_root),
         project_root + "/ansible/" + playbook])
--- src/configure_command.py
+++ src/configure_command.py
@@ -9,11 +9,16 @@ from subprocess import call
 def configure_aws(arg_vars, project_root):
   engraver_profile = expanduser("~") + "/.engraver"
   call(["touch", engraver_profile])
+
+  access_key = raw_input("AWS Access Key ID: ")
+  secret_key = raw_input("AWS Secret Access Key: ")
   aws_key_name = raw_input("AWS SSH key name: ")
   pem_file_path = raw_input("AWS SSH .pem file location: ")
 
   config = ConfigParser.ConfigParser()
   config.add_section('aws')
+  config.set('aws', 'aws_access_key', access_key)
+  config.set('aws', 'aws_secret_key', secret_key)
   config.set('aws', 'aws_key_name', aws_key_name)
   config.set('aws', 'pem_file_name', pem_file_path)
 
