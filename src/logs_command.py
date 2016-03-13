6af578f50e8c425335c4f3abde2429dc4514e07a
Michael Drogalis <mjd3089@rit.edu>
2016-03-12 19:13:52 -0800

    Cleans up logs command.

--- src/logs_command.py
+++ src/logs_command.py
@@ -6,7 +6,7 @@ import util
 
 from os.path import expanduser, exists
 from subprocess import call
-from colors import bcolors
+from colors import bcolors, print_ok, print_fail
 
 def stream_logs(arg_vars, project_root):
   config = ConfigParser.ConfigParser()
@@ -14,9 +14,10 @@ def stream_logs(arg_vars, project_root):
   config.read(engraver_profile)
   pem_file_path = config.get('aws', 'pem_file_name', 0)
   remote_user = config.get('aws', 'remote_user', 0)
-  container_name = arg_vars['service'] + "_container_name"
+  services = arg_vars['service']
+  container_name = service + "_container_name"
 
-  f = project_root + "/ansible/roles/" + arg_vars['service']
+  f = util.service_path(project_root, service)
   if util.verify_cluster_exists(arg_vars, project_root):
     if exists(f):
       with open(f + "/defaults/main.yml", "r") as stream:
@@ -25,6 +26,7 @@ def stream_logs(arg_vars, project_root):
           container = content[container_name]
           call(["ssh", "-t", "-i", pem_file_path, remote_user + "@" + arg_vars['host'], "docker logs -f " + container])
         else:
-          print(bcolors.FAIL + "> Service does not define container_name in defaults/main.yml. Cannot stream logs." + bcolors.ENDC)
+          base = "Service does not define {0}_container_name in defaults/main.yml of its Ansible role. Cannot stream logs."
+          print_fail(base.format(service))
     else:
-      print(bcolors.FAIL + "> Service not found." + bcolors.ENDC)
+      print_fail("Service not found.")
